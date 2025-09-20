e68396cd1436728aa3ca326162372c74d09e48ac
Jamie Kariuki <jamiekariuki36@gmail.com>
2025-09-20 08:42:46 +0300

    Version 1.4

--- wsgi.py
+++ wsgi.py
@@ -1,3 +1,10 @@
+import os
 from main import create_app
 
 application = create_app()
+
+if __name__ == "__main__":
+    application.run(
+        host=os.getenv("FLASK_HOST", "0.0.0.0"),
+        port=int(os.getenv("FLASK_PORT", 5000))
+    )
